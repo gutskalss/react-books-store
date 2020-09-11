1507900d291d7b6e8c951de40a9f3590c8084a3f
gutskalss <gutskalss@gmail.com>
2020-09-11 11:55:01 +0300

    Updates

---
+++ precache-manifest.d35889554018648103d75e6afc802d1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce28b4372fe4582b0c327662d7d6b4a5",
+    "url": "/react-books-store/index.html"
+  },
+  {
+    "revision": "789b474b949d303e426b",
+    "url": "/react-books-store/static/css/main.a9c9219f.chunk.css"
+  },
+  {
+    "revision": "7225bbd3f0429ef45247",
+    "url": "/react-books-store/static/js/2.67b957de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-books-store/static/js/2.67b957de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789b474b949d303e426b",
+    "url": "/react-books-store/static/js/main.5fc6aaa2.chunk.js"
+  },
+  {
+    "revision": "e71d1b57ae5c71927e2a",
+    "url": "/react-books-store/static/js/runtime-main.2da6eeb3.js"
+  }
+]);
